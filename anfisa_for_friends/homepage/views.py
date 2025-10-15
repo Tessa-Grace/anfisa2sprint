b31e539ff8006f2cae0be1b07ee399798c964829
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-10-15 20:34:41 +0200

    update

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,20 @@
+# Для применения Q-объектов их нужно импортировать:
+from django.db.models import Q
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Запрос:
+    ice_cream_list = (
+        IceCream.objects
+        .filter(is_on_main=True, is_published=True)
+        .order_by('title')
+        .values('id', 'title', 'description', 'wrapper__title')
+    )
+    context = {
+        # 'ice_cream_list': ice_cream_list,
+        'categories': categories
+    }
+    return render(request, template_name, context) 
